7f4ff0641ac367d3ab6e5dfd7d70ebc89ea42bf7
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-03-16 15:02:53 -0300

    javascript

---
+++ exercicios/js/index.js
@@ -0,0 +1,11 @@
+function getValue()
+{
+    var value = document.getElementById('name').value
+    document.getElementById('result').innerHTML = 'bem-vindo, ' + value + "!";
+}
+
+function removeValue()
+{
+    document.getElementById('name').value = "";
+    document.getElementById('result').innerHTML = '';
+}
